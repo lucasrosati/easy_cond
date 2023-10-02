ad6230f07777c187cccc938305dbbec57435b125
RonaldoTSM <ronaldotsm@outlook.com>
2023-10-02 20:12:16 -0300

    Ajuste no views.py

--- Login/views.py
+++ Login/views.py
@@ -3,13 +3,13 @@ from django.shortcuts import render
 # Create your views here.
 
 def pagina_de_login_view(request):
-    return render(request, 'login/Pagina_de_login.html')
+    return render(request, 'login/pagina_de_login.html')
 
 def cadastro_view(request):
-    return render(request, 'login/Cadastro.html')
+    return render(request, 'login/cadastro.html')
 
 def pagina_de_cadastro_view(request):
-    return render(request, 'login/Pagina_de_cadastro.html')
+    return render(request, 'login/cagina_de_cadastro.html')
 
 def pagina_inicial(request):
     return render(request, 'login/cadastro.html')
