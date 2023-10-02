113072d09b82cd8287ba3dd2305696da1584696d
RonaldoTSM <ronaldotsm@outlook.com>
2023-10-02 18:42:04 -0300

    ajsutes aplicativo Login

--- Login/admin.py
+++ Login/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from django.contrib import admin
+from .models import UserProfile  # Importe o modelo Person
+
+admin.site.register(UserProfile)  # Registre o modelo Person no admin
--- Login/models.py
+++ Login/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class UserProfile(models.Model):
+    nome = models.CharField(max_length=100)
+    nome_usuario = models.CharField(max_length=50)
+    cpf = models.CharField(max_length=14, unique=True)  # CPF deve ser único
+    apartamento = models.CharField(max_length=10)
+    tipo_usuario = models.CharField(max_length=20)
+
+    def _str_(self):
+        return self.nome
