bfb8cc754fe5609536c33a6cebdeddb39b88f794
RonaldoTSM <ronaldotsm@outlook.com>
2023-10-02 21:05:05 -0300

    Ajustes nos views.py

--- Reserva/views.py
+++ Reserva/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 
 def reserva_view(request):
-    return render(request, 'Reserva/reserva.html')
+    return render(request, 'reserva/reserva.html')
 
 def legenda_view(request):
-    return render(request, 'Reserva/legenda.html')
+    return render(request, 'reserva/legenda.html')
 
 def calendario_view(request):
-    return render(request, 'Reserva/calendario.html')
+    return render(request, 'reserva/calendario.html')
