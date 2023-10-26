f4b11e12f141c15b0d22f62811baca84cb3aa40c
RonaldoTSM <ronaldotsm@outlook.com>
2023-10-25 21:10:36 -0300

    Criação de novas telas

--- visitas/apps.py
+++ visitas/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class AgendamentoConfig(AppConfig):
+class VisitasConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "Agendamento"
+    name = "visitas"
