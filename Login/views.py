d6846a75ff7ba72161805f2fa6b81dfedaa3a5e8
joaonovaes1 <joaomnovaes1998@gmail.com>
2023-10-26 19:44:57 -0300

    setup do banco de dados

--- Login/views.py
+++ Login/views.py
@@ -1,9 +1,25 @@
 from django.shortcuts import render
+from Login.models import UserProfile
 
 # Create your views here.
 
 def pagina_de_login_view(request):
-    return render(request, 'login/pagina_de_login.html')
+    if request.method == 'POST':
+        print('Dados recebidos no servidor', request.POST)
+        nome = request.POST.get('nome')
+        nome_de_usuario = request.POST.get('user')
+        senha = request.POST.get('senha')
+        apartamento = request.POST.get('apartamento')
+        tipo_de_usuario = request.POST.get(tipoUsuario)
+
+
+
+#nome = nome
+#nome de usuario = user
+#senha = senha
+#apartamento = apartamento
+#tipo de usuario = tipoUsuario
+
 
 def cadastro_view(request):
     return render(request, 'login/cadastro.html')
