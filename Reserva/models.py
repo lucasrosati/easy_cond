e94715a2a790de54fc2dd598b0c56fb4d9758b1a
joaonovaes1 <joaomnovaes1998@gmail.com>
2023-10-29 15:42:14 -0300

    pasta e2e_test e modelos criados

--- Reserva/models.py
+++ Reserva/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
+
+ESPAÇO_CHOICES = (
+    ('salao_de_festas', 'Salão de Festas'),
+    ('campo_de_futebol', 'Campo de Futebol'),
+    ('espaco_gourmet', 'Espaço Gourmet'),
+    ('quadra_de_tenis', 'Quadra de Tênis'),
+)
+
 # Create your models here.
+class cadasto_reserva(models.Model):
+    espaco_comum = models.CharField(max_length=20, choices=ESPAÇO_CHOICES)
+    data_reserva = models.DateField()
+
+
+class Meta:
+    db_table = 'reserva_de_espaco'
--- e2e_tests/test_login.py
+++ e2e_tests/test_login.py
@@ -1,6 +1,53 @@
 from selenium import webdriver
+from django.test import LiveServerTestCase 
+from selenium.webdriver.common.by import By
+from django.contrib.auth.models import User 
+from selenium.webdriver.common.keys import Keys 
 
 
-def setup(self):
-    self.browser = webdriver.Chrome('e2e_tests/chromedriver.exe')
-    
+
+
+
+class teste_login(LiveServerTestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Chrome('e2e_tests/chromedriver.exe')
+            
+        
+        self.usuario = User.objects.create_user(
+            usuario='usuario_teste',
+            nome_usuario='RonaldoJacaré',
+            senha='12345678',
+            apart='100',
+            tipo_usuario='MORADOR')
+        self.browser.get("http://localhost:8000/")
+
+
+
+    def tearDown(self):
+        self.browser.close()
+
+    def teste_login_invalido(self):
+        self.browser = webdriver.Chrome('e2e_tests/chromedriver.exe')
+        self.browser.get('login/pagina_de_cadastro.html')
+        usuario = self.browser.find_element_by_xpath(By.XPATH, '//*[@id="cadastroForm"]/div[1]')
+        nome_usuario = self.browser.find_element_by_xpath(By.XPATH, '//*[@id="cadastroForm"]/div[2]')
+        senha = self.browser.find_element_by_xpath_(By.XPATH, '//*[@id="cadastroForm"]/div[3]')
+        apart = self.browser.find_element_by_xpath(By.XPATH, '//*[@id="cadastroForm"]/div[4]')
+        tipo_usuario = self.browser.find_element_by_xpath(By.XPATH, '//*[@id="cadastroForm"]/div[5]')
+        envio_dados = self.browser.find_element_by_xpath(By.XPATH, '//*[@id="cadastroForm"]/button')
+        
+
+        usuario.send_keys('usuario_teste')
+        nome_usuario.send_keys('RonaldoJacaré')
+        senha.send_keys('12345678')
+        apart.send_keys('100')
+        tipo_usuario.send_keys('MORADOR')
+        envio_dados.send_keys(Keys.RETURN)
+
+
+        assert 'dados incorretos' in self.browser.page_source
+
+
+
+
