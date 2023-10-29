80a5c52e2e3eea677b3004739dd254c150456843
lucasrosati <lucasrosati@hotmail.com>
2023-10-29 03:36:10 -0300

    db port update

--- EasyCond/settings.py
+++ EasyCond/settings.py
@@ -22,7 +22,7 @@ load_dotenv(BASE_DIR / '.env')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-TARGET_ENV = os.getenv('TARGET_ENV')
+TARGET_ENV = 'development'
 NOT_PROD = not TARGET_ENV.lower().startswith('prod')
 
 if NOT_PROD:
@@ -36,6 +36,7 @@ if NOT_PROD:
             'ENGINE': 'django.db.backends.postgresql',
             'NAME': os.environ.get('DBNAME'),
             'HOST': os.environ.get('DBHOST'),
+            'PORT': os.environ.get('DBPORT'),
             'USER': os.environ.get('DBUSER'),
             'PASSWORD': os.environ.get('DBPASSWORD'),
             'OPTIONS': {'sslmode': 'require'},
@@ -58,6 +59,7 @@ else:
             'ENGINE': 'django.db.backends.postgresql',
             'NAME': os.environ.get('DBNAME'),
             'HOST': os.environ.get('DBHOST'),
+            'PORT': os.environ.get('DBPORT'),
             'USER': os.environ.get('DBUSER'),
             'PASSWORD': os.environ.get('DBPASSWORD'),
             'OPTIONS': {'sslmode': 'require'},
@@ -171,5 +173,3 @@ STATICFILES_STORAGE = ('whitenoise.storage.CompressedManifestStaticFilesStorage'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
-#teste
