8f716a48d332a3295b6cc8f3078eae46c45ba407
RonaldoTSM <ronaldotsm@outlook.com>
2023-10-29 00:44:37 -0300

    ajuste na pasta views

--- visitas/views.py
+++ visitas/views.py
@@ -1,4 +1,4 @@
 from django.shortcuts import render
 
 def visitas_view(request):
-    return render(request, 'visitas/cadastro_visitas.html') 
+    return render(request, 'visitas/cadastro_visitas.html')
