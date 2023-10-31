3570832d264486360b3d1dee96287e1bfdf70c36
RonaldoTSM <ronaldotsm@outlook.com>
2023-10-30 23:44:09 -0300

    criação do bd de visitas

--- Login/admin.py
+++ Login/admin.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 
 # Register your models here.
 
-from django.contrib import admin
 from .models import UserProfile  # Importe o modelo Person
 
 admin.site.register(UserProfile)  # Registre o modelo Person no admin
--- visitas/views.py
+++ visitas/views.py
@@ -1,25 +1,31 @@
 from django.shortcuts import render, redirect
 from .models import visita
+from django.contrib import messages
 
 def cadastro_visita_view(request):
     if request.method == 'POST':
         nome = request.POST.get('nome')
         cpf = request.POST.get('cpf')
-        tipo_usuario = request.POST.get('tipo_usuario')  # Ajuste o nome do campo para corresponder ao HTML
+        tipo_usuario = request.POST.get('tipo_usuario')
 
         if not nome or not cpf or not tipo_usuario:
-            # Adicione algum código de manipulação de erro aqui
-            return render(request, 'cadastro_visita.html', {'error': 'Campos obrigatórios não preenchidos'})
-
-        visita.objects.create(
-            nome=nome,
+            messages.error(request, 'Todos os campos são obrigatórios')
+            return render(request, 'visitas/cadastro_visitas.html')
+        
+        nova_visita, created = visita.objects.get_or_create(
             cpf=cpf,
-            tipo_usuario=tipo_usuario
+            defaults={'nome': nome, 'tipo_usuario': tipo_usuario}
         )
 
-        return redirect('/menu/')  # Redirecione para o menu após o cadastro bem-sucedido
+        if created:
+            messages.success(request, 'Cadastro realizado com sucesso!')
+        else:
+            messages.info(request, 'Visitante já estava cadastrado. Informações atualizadas.')
 
-    return render(request, 'cadastro_visita.html')
+        return redirect('menu')
 
-def visitas_view(request):
+    return render(request, 'visitas/cadastro_visitas.html')
+
+
+def visitas_view(request):    
     return render(request, 'visitas/cadastro_visitas.html')
